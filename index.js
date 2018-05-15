4736a62f6c33e020a2547bc85e74290f4bf8b5fe
= <=>
2018-05-15 15:22:16 -0400

    notes has some build details now.. just need to make them as items

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const rules = {
     RUN_REPL: false,
-    RUN_WEBSERVER: true,
-    RUN_TELNETSERVER: true,
+    RUN_WEBSERVER: false,
+    RUN_TELNETSERVER: false,
     RUN_TEST_SCRIPTS: false
 }
 const state = require('./app')(rules);
