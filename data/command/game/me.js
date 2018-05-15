a943ea2bda149738161d32b45de02b64b44a6664
= <=>
2018-05-15 14:46:01 -0400

    ansi up!!! this is the bestttttt!

--- data/command/game/me.js
+++ data/command/game/me.js
@@ -12,17 +12,17 @@ module.exports = (repo, state) => ({
     execute: (client, str) => {
         const player = client.player;
         client.log(`${player.name} details`);
-        client.log('Your vitals--------------------');
+        client.log('Your vitals -------------------------------');
         client.log(`Health:                     ${pad(player.vitals.health,3)} / ${player.vitals.healthMax}`);
         client.log(`Stamina:                    ${pad(player.vitals.stamina,3)} / 100`);
         client.log(`Hunger:                     ${pad(100 - player.vitals.hunger,3)} / 100`);
 
-        client.log('Your stats/skills---------------');
+        client.log('Your stats/skills -------------------------');
         client.log(`Assembly/ Dissasembly:      ${pad(~~(player.skills.assembly), 3)} / 100 (+ ${player.skills.totalAssembly() - player.skills.assembly})`);
         client.log(`Detection:                  ${pad(~~(player.skills.detection), 3)} / 100 (+ ${player.skills.totalDetection() - player.skills.detection})`);
         client.log(`Research:                   ${pad(~~(player.skills.research), 3)} / 100 (+ ${player.skills.totalResearch() - player.skills.research})`);
 
-        client.log('Your equipment and inventory-----');
+        client.log('Your equipment and inventory --------------');
         client.log(`Hand:                       ${client.player.equipment.wield().fullName()}`);
         client.log(`Head:                       ${client.player.equipment.helmet().fullName()}`);
         client.log(`Armor:                      ${client.player.equipment.armor().fullName()}`);
--- web/main.js
+++ web/main.js
@@ -1,29 +1,15 @@
-var cursorType = 'underscore';
-var systemPrompt = 'Game:\\';
-var fuzzy = false;
-
+var ansi_up = new AnsiUp;
 var terminal = document.getElementById('terminal');
 var prompt = document.getElementById('prompt');
-var promptWrapper = document.getElementById('promptWrapper');
-var promptCopy = document.getElementById('promptCopy');
+var fullHistory = '';
 
-var setFocus = function() {
-    prompt.focus();
-}
-
-var setCopy = function(value) {
-    // promptCopy.innerHTML = '<span id="systemPrompt">' + systemPrompt + '</span>' + value;
-    // promptCopy.innerHTML+='<span id="cursor-' + cursorType + '"></span>';
-};
+var setFocus = function() { prompt.focus(); }
 
 var newLine = function(str) {
-    terminal.innerHTML += '<br>' + str;
-    console.log(document.body.scrollHeight, window.innerHeight)
-    setTimeout(() => {
-        if(document.body.scrollHeight > window.innerHeight) {
-            window.scrollTo(0,document.body.scrollHeight);
-        }
-    }, 100);
+    fullHistory += '\r\n' + str;
+    var html = ansi_up.ansi_to_html(fullHistory);
+    terminal.innerHTML = html;
+    terminal.scrollTop = terminal.scrollHeight;
 };
 
 document.addEventListener('click', function() {setFocus() });
@@ -36,75 +22,9 @@ prompt.addEventListener('keypress', function(e) {
     }
 });
 
-if(fuzzy) { document.body.className = 'fuzzy'; }
-setCopy('');
-setFocus();
-
 socket.on('newLine', function(data) {
     let formatted = data;
-    
-    formatted = formatted.split('\n').join('<br>');
-    formatted = formatted.split('\t').join('&nbsp;&nbsp;&nbsp;&nbsp;');
-    // todo: make a pattern matcher
-    // todo: let it escape on its own
-    formatted = formatted.split(' ').join('&nbsp;');
-    formatted = formatted.split('[36m').join('<span class="color-36m">');
-    formatted = formatted.split('[39m').join('<span class="color-39m">');
-    formatted = formatted.split('[33m').join('<span class="color-33m">');
-    formatted = formatted.split('[32m').join('<span class="color-32m">');
-    formatted = formatted.split('[31m').join('<span class="color-31m">');
-
-    let count = (formatted.match(/<span/g) || []).length;
-
-    for(let e=0;e<count;e++) {
-        formatted+='</span>';
-    }
     newLine(formatted);
 });
 
-
-
-
-
-var colorReplace = function( input, replace ) {
-    var replaceColors = {
-        "0;31" : "{r",
-        "1;31" : "{R",
-
-        "0;32" : "{g",
-        "1;32" : "{G",
-
-        "0;33" : "{y",
-        "1;33" : "{Y",
-
-        "0;34" : "{b",
-        "1;34" : "{B",
-
-        "0;35" : "{m",
-        "1;35" : "{M",
-
-        "0;36" : "{c",
-        "1;36" : "{C",
-
-        "0;37" : "{w",
-        "1;37" : "{W",
-
-        "1;30" : "{*",
-
-        "0" : "{x"
-    };
-
-    if ( replace )
-    {
-        for( k in replaceColors )
-        {
-            var re = new RegExp( "\033\[[" + k + "]*m", "g" );
-
-            input = input.replace( re, replaceColors[ k ] );
-        }
-    } else {
-        input = input.replace( /\033\[[0-9;]*m/g, "" );
-    }
-
-    return input;
-};
+setFocus();
